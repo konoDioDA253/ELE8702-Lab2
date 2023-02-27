f1187fdbe00d4bd3e1cd947e0a09ea5f031cc2dd
Bouh <bouh.abdillahi@polymtl.ca>
2023-02-26 19:41:20 -0500

    clean start

--- test.py
+++ test.py
@@ -3,9 +3,11 @@ import pyautogui
 
 # Constitue le main
 
-formula_3GPP(0,0,0,0,00,0,0,0,0)
-reponse = None
-reponse = formula_3GPP(0,0,0,0,0,0,0,0,0)
-print("PL = ",reponse)
-my_variable = 2
+# formula_3GPP(0,0,0,0,00,0,0,0,0)
+# reponse = None
+# reponse = formula_3GPP(0,0,0,0,0,0,0,0,0)
+# print("PL = ",reponse)
+# my_variable = 2
 
+import os
+print(os.sep)
