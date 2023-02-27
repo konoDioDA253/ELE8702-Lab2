5ba78ab175b000435b226250d96be5f04be20fca
Bouh <bouh.abdillahi@polymtl.ca>
2023-02-26 20:05:51 -0500

    created branch2

--- test.py
+++ test.py
@@ -9,5 +9,26 @@ import pyautogui
 # print("PL = ",reponse)
 # my_variable = 2
 
-import os
-print(os.sep)
+# Python code to demonstrate working of
+# Deleting all occurrences of character
+# Using translate()
+
+# Python3 code to demonstrate working of
+# Deleting all occurrences of character
+# Using replace()
+ 
+# initializing string
+test_str = "GeeksforGeeks"
+ 
+# initializing removal character
+rem_char = "e"
+ 
+# printing original string
+print("The original string is :" + str(test_str))
+ 
+# Using replace()
+# Deleting all occurrences of character
+res = test_str.replace(rem_char, "")
+ 
+# printing result
+print("The string after character deletion : " + str(res))
